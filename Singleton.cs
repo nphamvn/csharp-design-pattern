0d7e463c6b5145ceae5d09d58c992ff55f6d2ce7
Nam Pham <pvnam95@hotmail.com>
2022-02-11 17:52:29 +0900

    Thread-safe singleton using lock

--- Singleton.cs
+++ Singleton.cs
@@ -1,14 +1,18 @@
 public sealed class Singleton
 {
     private static int counter = 0;
+    private static readonly object Instancelock = new object();
     private static Singleton instance = null;
     public static Singleton GetInstance
     {
         get
         {
-            if (instance == null)
-                instance = new Singleton();
-            return instance;
+            lock (Instancelock)
+            {
+                if (instance == null)
+                    instance = new Singleton();
+                return instance;
+            }
         }
     }
     private Singleton()
