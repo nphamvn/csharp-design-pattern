8a778aaf147f6de76ec802110d972bbec6fe8610
Nam Pham <pvnam95@hotmail.com>
2022-02-12 23:28:10 +0900

    Implemented Facade Design Pattern Example

---
+++ OnlineRestaurant.cs
@@ -0,0 +1,16 @@
+public class OnlineRestaurant
+{
+    private readonly List<Order> _cart;
+    public OnlineRestaurant()
+    {
+        _cart = new List<Order>();
+    }
+    public void AddOrderToCart(Order order)
+    {
+        _cart.Add(order);
+    }
+    public void CompleteOrders()
+    {
+        Console.WriteLine("Orders completed. Dispatch in progress...");
+    }
+}
